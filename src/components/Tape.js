2f684732b28ba827449da8019a32ccec83dc517a
Jake <jakezuke@gmail.com>
2020-01-09 15:18:28 -0500

    fixed mobile behavior

--- src/components/Tape.js
+++ src/components/Tape.js
@@ -5,7 +5,7 @@ const Tape = (props) => {
 	return (
 		<>
 			<div className="show-desktop line" style={{top:`${props.position.y + 75}px`, left: `${props.position.x - 35}px`}}></div>
-			<div className="show-mobile line" style={{top:`${props.position.y + 20}px`, left: `${props.position.x - 35}px`}}></div>
+			<div className="show-mobile line" style={{top:`${props.position.y + 10}px`, left: `${props.position.x - 35}px`}}></div>
 		</>
 	)
 }
